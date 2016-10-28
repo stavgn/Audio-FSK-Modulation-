6ece0709ac1e414c3f3e7b403a243c6dba3a386d
Stav Green <stavgn@gmail.com>
2016-10-28 18:31:53 +0300

    Fixes and updates

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import sys
 payload1 = payload.Payload(sys.argv[1])
 signal1 = signal.Signal(payload1)
 signal1.modulateSignal(modulate.FSK)
-signal1.generateAudiofile(10)
+signal1.generateAudiofile(5)
 #signal1.plotSignal(0,2000)
